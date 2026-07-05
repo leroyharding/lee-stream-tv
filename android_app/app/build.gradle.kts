dee9257794bd16f4391098ec25e91b0f0dcf8089
leroyharding <leroyharding@gmail.com>
2026-07-06 00:53:05 +0100

    v1.6.8 Hotfix: Fixed AD History playback parsing and external player intent routing

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.leestreamtv"
         minSdk = 24
         targetSdk = 36
-        versionCode = 21
-        versionName = "1.6.7"
+        versionCode = 22
+        versionName = "1.6.8"
     }
 
     buildTypes {
