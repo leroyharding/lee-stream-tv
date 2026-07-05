82b1042a363dab105b75f966af6ffd4adde8f654
leroyharding <leroyharding@gmail.com>
2026-07-06 00:00:27 +0100

    v1.6.6 Hotfix: Fixed Firestick spatial navigation for All-Debrid modal

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.leestreamtv"
         minSdk = 24
         targetSdk = 36
-        versionCode = 19
-        versionName = "1.6.5"
+        versionCode = 20
+        versionName = "1.6.6"
     }
 
     buildTypes {
