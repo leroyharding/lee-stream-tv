3d2c478b27b6635f526eb10ed8bb96c5882df775
leroyharding <leroyharding@gmail.com>
2026-07-06 01:46:59 +0100

    release: v1.7.0

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.leestreamtv"
         minSdk = 24
         targetSdk = 36
-        versionCode = 23
-        versionName = "1.6.9"
+        versionCode = 24
+        versionName = "1.7.0"
     }
 
     buildTypes {
